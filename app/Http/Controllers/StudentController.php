0631bd64844dde48239ed86491cc95da8ee49df3
PayeJohn2022 <114854688+PayeJohn2022@users.noreply.github.com>
2023-05-01 20:14:37 +0800

    Second Repo in Midterm Exam

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -43,7 +43,7 @@ class StudentController extends Controller
                 'firstname' => 'required|alpha:max:50',
                 'middlename' => 'required|alpha:max:50',
                 'bday' => 'date',
-                'age' => 'required|int:gt:50 years'
+                'age' => 'required|int:lt:50 years'
             ])  
         ];
         $this->validate($request, $rules);
